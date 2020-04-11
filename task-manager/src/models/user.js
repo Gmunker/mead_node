0d7fa5f046111502d4f77254c7aba1485a5a8392
Greg Munker <gmunker@caesars.com>
2020-04-11 11:04:00 -0700

    ADDED: docker-compose for mongodb; mongoose models for users and task

---
+++ task-manager/src/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trum: true
+  },
+  password: {
+    type: String,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot include "Password or password"');
+      }
+    }
+  },
+  email: {
+    type: String,
+    reuqire: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("email is not vaild");
+      }
+    }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
+  }
+});
+
+module.exports = User;
