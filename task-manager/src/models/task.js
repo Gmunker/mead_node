b5cb9e2a34a9c5db436238ac3f51f69a8daae144
Greg Munker <gmunker@gmail.com>
2021-01-10 22:44:26 -0700

    enabled options for get tasks with completed status, pagination of results

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,7 +1,7 @@
 const { ObjectID } = require('mongodb');
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
   description: {
     type: String,
     require: true,
@@ -16,6 +16,11 @@ const Task = mongoose.model('Task', {
     required: true,
     ref: 'User'
   },
-});
+},
+{
+  timestamps: true
+})
+
+const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task
