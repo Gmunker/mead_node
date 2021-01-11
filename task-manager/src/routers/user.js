157000ebe176c33b59b68f8fe3f6628898a5115b
Greg Munker <gmunker@gmail.com>
2021-01-11 16:04:21 -0700

    added avatar upload handling

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -84,9 +84,20 @@ router.delete('/users/me', auth, async (req, res) => {
 });
 
 const upload = multer({
-  dest: 'avatar'
+  dest: 'images',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload a .jpg, .jpeg, or .png only'))
+    }
+    cb(undefined, true)
+  }
 })
+
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+
   res.send()
 })
 
