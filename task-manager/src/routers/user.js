3bcf4b8a93e0477a9825dda7c159ce019eda48af
Greg Munker <gmunker@gmail.com>
2021-01-11 21:53:41 -0700

    added sharp for handling avatar size and type

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer')
+const sharp = require('sharp')
 const auth = require('../middleware/auth')
 const User = require('../models/user');
 
@@ -96,7 +97,8 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-  req.user.avatar = req.file.buffer
+  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+  req.user.avatar = buffer
   await req.user.save()
   res.send()
 }, (error, req, res, next) => {
@@ -119,7 +121,7 @@ router.get('/users/:id/avatar', async (req, res) => {
     if (!user || !user.avatar) {
       throw new Error
     }
-    res.set('Content-Type', 'image/jpg')
+    res.set('Content-Type', 'image/png')
     res.send(user.avatar)
   } catch (e) {
     res.status(404).send()
