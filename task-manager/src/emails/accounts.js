9fb28cf1eb0645be169dc8dd40900ce44a6c2070
Greg Munker <gmunker@gmail.com>
2021-01-17 16:01:25 -0700

    updated email addresses

--- task-manager/src/emails/accounts.js
+++ task-manager/src/emails/accounts.js
@@ -5,16 +5,22 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'gmunker@gmail.com',
+    from: 'gmunker@vitalityhandmade.com',
     subject: 'Thanks joining in!',
     text: `Welcome to the app ${name}, let me know how you like it.`
-  })
+  }).then(() => { }, error => {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  });
 }
 
 const sendGoodbyeEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'gmunker@gmail.com',
+    from: 'gmunker@vitalityhandmade.com',
     subject: 'So sorry to see you go',
     text: 'Sorry to see you go, please let me know if we can improve.'
   })
